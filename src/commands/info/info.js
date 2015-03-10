db537df5c647ae1ab0725246fa9e1b99a5f6159f
Valeriy <valeriy.sorokobatko@gmail.com>
2015-03-10 19:45:57 +0200

    [update] info command. logs using cli-table and prettyjson

--- src/commands/info/info.js
+++ src/commands/info/info.js
@@ -2,8 +2,10 @@
 
 var Command = require('../command'),
     path = require('path'),
+    prettyjson = require('prettyjson'),
     Chalk = require('chalk'),
     Types = require('../../utils/types'),
+    Table = require('cli-table'),
     Placeholders = require('../create/placeholders'),
     SchemaClass = require('../create/schema'),
     Info = Command.extend({
@@ -24,104 +26,144 @@ var Command = require('../command'),
             .then(this.detailed.bind(this));
       },
 
+      highlite: function(path) {
+        return path.replace(/(:[^-|\/|\.]+)/g, Chalk.green('$1'));
+      },
+
       detailed: function() {
         if (this.component) {
-          var Schema = new SchemaClass(this.config.app_config.components);
+          var Schema = new SchemaClass(this.config.app_config.components, this.config.app_config.base);
 
-          var components = this.config.app_config.components;
+          var component = Schema.get(this.component);
 
-          if (!components.hasOwnProperty(this.component)) {
-            throw new Error('Component "' + this.component + '" does not exists');
-          }
+          var data = [];
+          var provide = [];
+          var dependencies = [];
 
-          var component = components[this.component];
-
-          console.log('');
-          console.log(Chalk.blue('+--------------'));
           if (Types.isString(component)) {
-
-
             var name = this.component;
-            console.log('| component: ' + Chalk.cyan(name));
+            data.push({component: Chalk.cyan(name)});
 
             var path = Schema.resolve(this.component);
+            var map = Placeholders.parse(path);
 
-            var map = Placeholders.parse(path).join(':');
-            console.log('| parameters map: ' + Chalk.green(map));
-
-            console.log('| path: ' + path);
-
-          } else if (Types.isObject(component)) {
-
-            console.log('| component: ' + Chalk.cyan(this.component));
-            console.log('| parameters map: ' + Chalk.green(component.map));
-
-            if (component.path) {
-              var path = Schema.resolve(this.component);
-              console.log('| path: ' + path);
+            if (map) {
+              data.push({parameters: map.join(':')});
             }
 
+            data.push({path: this.highlite(path)});
+          } else if (Types.isObject(component)) {
+
+            data.push({component: this.component});
+
+            if (component.map) {
+              data.push({parameters: component.map});
+            }
+
+            if (component.template) {
+              data.push({template: component.template});
+            }
+
+            if (component.path) {
+              data.push({path: Schema.resolve(this.component)});
+            }
 
             if (Types.isObject(component.provide)) {
-              console.log('| ');
-              console.log('| provided placeholders:');
-
               for (var placeholder in component.provide) {
                 if (component.provide.hasOwnProperty(placeholder)) {
                   var value = component.provide[placeholder];
 
                   if (value[0] == ':') {
-                    console.log('|  - ' + Chalk.yellow(placeholder) + ' value will equal to ' + Chalk.green(value.slice(1)));
+                    provide.push([Chalk.green(':' + placeholder), 'equals to ' + Chalk.green(':' + value.slice(1))]);
                   } else {
-                    console.log('|  - ' + Chalk.yellow(placeholder) + ' = ' + value);
+                    provide.push([placeholder, value]);
                   }
                 }
               }
             }
 
-
             if (Types.isArray(component.components)){
-              console.log('| ');
-              console.log('| relative components: ');
-
               for (var i = 0, size = component.components.length; i < size; i++) {
                 var item = component.components[i].split(':'),
                     name = item[0],
                     template = item[1] || 'default';
 
-                console.log('|  - ' + Chalk.green(name) + ' using ' + Chalk.underline(template) + ' template');
+                var path = Schema.resolve(name);
+
+                dependencies.push([Chalk.yellow(name), this.highlite(path), template]);
               }
             }
           }
 
-          console.log(Chalk.yellow('+--------------'));
+
+          var innerTableChars = {
+            chars: {
+              'top': '' ,
+              'top-mid': '' ,
+              'top-left': '' ,
+              'top-right': '',
+              'bottom': '' ,
+              'bottom-mid': '' ,
+              'bottom-left': '' ,
+              'bottom-right': '',
+              'left': '' ,
+              'left-mid': '' ,
+              'mid': '─' ,
+              'mid-mid': '',
+              'right': '' ,
+              'right-mid': '' ,
+              'middle': '│'
+            }
+          };
+
+
+          if (provide.length) {
+            var provideTable = new Table(innerTableChars);
+            provideTable.push.apply(provideTable, provide);
+            data.push({provide: provideTable.toString()});
+          }
+
+          if (dependencies.length) {
+            var depsTable = new Table(innerTableChars);
+            depsTable.push.apply(depsTable, dependencies);
+            data.push({'component list': depsTable.toString()});
+          }
+
+          var table = new Table();
+          table.push.apply(table, data);
+
           console.log('');
+          console.log('');
+
+          if (!data.hasOwnProperty('map') && !data.hasOwnProperty('provide')) {
+            console.log(Chalk.red('Warning: @map or @provide should be described. ' +
+            'Otherwise - enchup will not be able to compile component paths and template'));
+          }
+
+
+          console.log(table.toString());
         }
       },
 
       info: function() {
 
-        var config = this.config;
+        var app = this.config.app_config || {};
+        var user = this.config.user_config || {};
 
-        if (config.app_config.hasOwnProperty('name')) {
-          console.log('name: ' + config.app_config.name);
-        }
+        var infoBlock = {
+          author: {
+            app: app.author,
+            user: user.author
+          },
+          application: app.application,
+          version: app.version
+        };
 
-        if (config.app_config.hasOwnProperty('author')) {
-          console.log('author: ' + config.app_config.author);
-        }
-
-        if (config.app_config.hasOwnProperty('version')) {
-          console.log('version: ' + config.app_config.version);
-        }
-
-        if (config.app_config.hasOwnProperty('description')) {
-          console.log('description: ' + config.app_config.description);
-        }
-
-        if (config.app_config.hasOwnProperty('readme')) {
-          console.log('readme: ' + config.app_config.readme);
-        }
+        console.log(prettyjson.render(infoBlock));
+        console.log('');
+        console.log(prettyjson.render({
+          components: Object.keys(app.components)
+        }));
       }
     });
 
